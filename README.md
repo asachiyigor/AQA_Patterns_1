33bd2d81c0b0efb6ab5535f4906ae5c3798d9410
asachiyigor <asachiyigor@gmail.com>
2022-05-23 15:20:51 +0300

    add correct 1. README.md 2. delete annotation @Data 3. ArrayList instead of Set 4. Duration instead of Properties 5. add new test 6. delete if from tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 PATTERNS [![Build status](https://ci.appveyor.com/api/projects/status/7a68tvoakjb2x1gc?svg=true)](https://ci.appveyor.com/project/asachiyigor/aqa-patterns-1-nfv8w)
 
 * ¬рем€, затраченное на ручное тестирование (минут): 30
-* ¬рем€, затраченное на автоматизацию (минут): 150
+* ¬рем€, затраченное на автоматизацию (минут): 15
--- src/test/java/ru/netology/test/CardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/CardDeliveryOrderTest.java
@@ -1,4 +1,4 @@
-package ru.netology.Test;
+package ru.netology.test;
 
 import com.codeborne.selenide.Configuration;
 import com.github.javafaker.Faker;
@@ -6,14 +6,16 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.Data.DataGenerator;
+import ru.netology.data.DataGenerator;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryOrderTest {
@@ -30,6 +32,10 @@ public class CardDeliveryOrderTest {
             .plusDays(secondDay)
             .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
+    String IncorrectDayDelivery = LocalDate.now()
+            .plusDays(1)
+            .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+
     @BeforeAll
     static void setUpAll() {
         faker = new Faker(new Locale("ru"));
@@ -38,28 +44,21 @@ public class CardDeliveryOrderTest {
 
     @BeforeEach
     void setUp() {
-        Configuration.timeout = 15000;
         open("http://localhost:9999/");
     }
 
     @Test
-    void shouldSuccessfulPlanMeeting() throws IOException {
+    void shouldShowErrorWithIncorrectData() throws IOException {
 
         $("[placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").sendKeys(firstDayDelivery);
+        $("[data-test-id='date'] input").sendKeys(IncorrectDayDelivery);
         $("[name='name']").setValue(DataGenerator.generateName());
         $("[name='phone']").setValue(DataGenerator.generatePhone());
-        $("[data-test-id='agreement']").click();
         $("[placeholder='Город']").setValue(DataGenerator.generateCity());
+        $("[data-test-id='agreement']").click();
         $(".button").click();
-        String cityPoint = (String) $(".input__sub").text();
-        if (cityPoint.equals("Доставка в выбранный город недоступна")) {
-            $("[placeholder='Город']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-            $("[placeholder='Город']").setValue(DataGenerator.setNewCity());
-            $(".button").click();
-        }
-        $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на "
-                + firstDayDelivery));
+        $("[data-test-id='date'].input_invalid.input__sub")
+                .$(withText("Заказ на выбранную дату невозможен"));
     }
 
     @Test
@@ -69,15 +68,9 @@ public class CardDeliveryOrderTest {
         $("[data-test-id='date'] input").sendKeys(firstDayDelivery);
         $("[name='name']").setValue(DataGenerator.generateName());
         $("[name='phone']").setValue(DataGenerator.generatePhone());
-        $("[data-test-id='agreement']").click();
         $("[placeholder='Город']").setValue(DataGenerator.generateCity());
+        $("[data-test-id='agreement']").click();
         $(".button").click();
-        String cityPoint = (String) $(".input__sub").text();
-        if (cityPoint.equals("Доставка в выбранный город недоступна")) {
-            $("[placeholder='Город']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-            $("[placeholder='Город']").setValue(DataGenerator.setNewCity());
-            $(".button").click();
-        }
         $("[data-test-id='success-notification']").shouldHave(text("Встреча успешно запланирована на "
                 + firstDayDelivery));
         $("[data-test-id=date] input").doubleClick().sendKeys(Keys.BACK_SPACE);
@@ -85,6 +78,6 @@ public class CardDeliveryOrderTest {
         $$("button").find(exactText("Запланировать")).click();
         $("[data-test-id='replan-notification']").shouldBe(visible);
         $$("button").find(exactText("Перепланировать")).click();
-        $("div.notification__content").shouldBe(visible,exactText("Встреча успешно запланирована на " + secondDayDelivery));
+        $("div.notification__content").shouldBe(visible, Duration.ofSeconds(3)).shouldHave(text("Встреча успешно запланирована на " + secondDayDelivery));
     }
 }
